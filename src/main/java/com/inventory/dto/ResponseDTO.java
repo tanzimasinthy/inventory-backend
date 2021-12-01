4210c5740bf156455b570875705f5d68b52c9155
Nahid Hasan Nomro <52489202+nahidhasan94@users.noreply.github.com>
2021-12-01 11:09:24 +0600

    status added

--- src/main/java/com/inventory/dto/ResponseDTO.java
+++ src/main/java/com/inventory/dto/ResponseDTO.java
@@ -2,8 +2,9 @@ package com.inventory.dto;
 
 public class ResponseDTO<T> {
 
-    private T data;
+   private T data;
     private String message;
+    private String status;
 
     public ResponseDTO() {
     }
@@ -11,12 +12,14 @@ public class ResponseDTO<T> {
     public ResponseDTO generateSuccessResponse(T data, String message) {
         this.data = data;
         this.message = message;
+        this.status = "success";
         return this;
     }
 
     public ResponseDTO generateErrorResponse(String message) {
         this.data = null;
         this.message = message;
+        this.status = "error";
         return this;
     }
 
@@ -34,5 +37,13 @@ public class ResponseDTO<T> {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
